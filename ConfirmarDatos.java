3475f14966b0bc266296b88d76210d3fa38aed2e
DIEGO SALVA <diegosalvam@gmail.com>
2016-05-24 00:13:58 -0500

    Versión 1

--- ConfirmarDatos.java
+++ ConfirmarDatos.java
@@ -38,8 +38,7 @@ public class ConfirmarDatos extends AppCompatActivity {
         tvEmail.setText((CharSequence) bndRecuperar.get(getResources().getString(R.string.emailKey)));
         tvDescripcion.setText((CharSequence) bndRecuperar.get(getResources().getString(R.string.descripcionKey)));
 
-        //tvFecha.setText((CharSequence) bndRecuperar.get("fechaKey"));
-
+        
         bEditar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
